f51563e1884029b3c2ab30afc82aaaaf07c38b87
Dmytro-Kruhlov <129300009+Dmytro-Kruhlov@users.noreply.github.com>
2026-03-24 14:47:13 +0200

    Build(bd): add Comment model (#1)

--- comments/models.py
+++ comments/models.py
@@ -1,1 +1,20 @@
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Comment(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    homepage = models.URLField(blank=True, null=True)
+    text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    parent = models.ForeignKey(
+        "self", null=True, blank=True, related_name="replies", on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ["created_at"]
+
+    def __str__(self):
+        return f"{self.username}: {str(self.text)[:20]}"
